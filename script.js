d200ae3a91d93f1103804b5228672e63dc4426d0
Jihoo125 <109279349+Jihoo125@users.noreply.github.com>
2023-02-28 20:10:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ async function sendMessage(message) {
 			"Authorization": "Bearer YOUR_API_KEY_HERE"
 		},
 		body: JSON.stringify({
-			prompt
+			prompt: message,
+			max_tokens: 50,
+			n: 1,
+			stop: "\n",
+		}),
+	});
+	const data = await response.json();
+	return data.choices[0].text.trim();
+}
+
+// This function creates a chat bubble with the given message and adds it to the chat history
+function addChatBubble(message, isUser) {
+	const bubble = document.createElement("div");
+	bubble.className = isUser ? "user-bubble" : "bot-bubble";
+	bubble.innerText = message;
+	chatHistory.appendChild(bubble);
+	chatHistory.scrollTop = chatHistory.scrollHeight;
+}
+
+// This function handles user input and sends a message to the GPT-2 API
+async function handleInput() {
+	const message = userInput.value.trim();
+	if (message.length > 0) {
+		addChatBubble(message, true);
+		userInput.value = "";
+		const
